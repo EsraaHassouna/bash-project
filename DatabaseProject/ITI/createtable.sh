416d52df4d904d1a8bba14e8df64103c1cf2e3e9
Esraa Hassouna <esraa.hassouna9.11@gmail.com>
2022-12-21 18:53:06 +0200

    createtable

--- DatabaseProject/ITI/createtable.sh
+++ DatabaseProject/ITI/createtable.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
+ 
+let flag=0
 
-read tablename 
+while [[ $flag -eq 0 ]]
+do
+	echo "enter table name"
+	read tname 
 
-if [[-f "./$tablename"]]
-then 
-	cd $tablename.sh
-else 
-	touch $tablename.sh
-fi 
-./tablename 
+	if [[ -f $tname ]];
+	then 
+		echo "Name exisits! choose another name for the table "
+	else
+		touch $tname
+		touch tname_meta
+		(( flag++ ))
+	fi 
+done
+echo "Column;Datatype;PK" >> tname_meta
+
+typeset -i cnumber
+let l=1
+let pk=1 #primary key exist 
+
+read -p "Enter the No. of columns" cnumber
+
+while [ $l -le $cnumber ]
+do
+	echo "Line No.$l"	
+	echo "enter the column data as follows:"
+	echo "Column;Datatype;PK"
+	read metadata
+	echo metadata >> tname_meta
+	(( l++ ))
+
+done
+
 
---
+++ DatabaseProject/createTable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "please enter the table name"
+
+read tName
+
+if [[-f $tName ]];
+then
+	echo "choose another table name"
+
+fi
+
+
