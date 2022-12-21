ff74dedfdb6157317ff4abbf5238c86fa5a2291c
RahmaEl-Sheikh <eng.rahma.elsheikh@gmail.com>
2022-12-21 20:08:09 +0200

    list-script

---
+++ DatabaseProject/ITI/list.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+subdircount=$(find . -not -path '*/.*' | wc -l)
+if (("$subdircount" == 1 ))
+then
+ echo " This table does not exist"
+ cd ..
+else
+ echo "Displaying your List"
+ls
+cd ..
+fi
