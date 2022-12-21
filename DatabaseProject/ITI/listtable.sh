6be49c88be382d8232f2146bf02dd7a6070bda21
RahmaEl-Sheikh <eng.rahma.elsheikh@gmail.com>
2022-12-21 20:20:06 +0200

    listtable-script

--- DatabaseProject/ITI/listtable.sh
+++ DatabaseProject/ITI/listtable.sh
@@ -2,11 +2,11 @@
 
 subdircount=$(find . -not -path '*/.*' | wc -l)
 if (("$subdircount" == 1 ))
-then
- echo " This table does not exist"
+ then
+     echo " This table does not exist"
  cd ..
 else
- echo "Displaying your List"
+     echo "Displaying your List"
 ls
 cd ..
 fi
