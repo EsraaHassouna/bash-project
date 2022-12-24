8f664161e6ce4eb0088914ceb059da325507a070
Esraa Hassouna <esraa.hassouna9.11@gmail.com>
2022-12-24 23:44:28 +0200

    deleted datasases

--- DatabaseProject/ITI/createtable.sh
+++ DatabaseProject/ITI/createtable.sh
@@ -16,26 +16,16 @@ do
 		(( flag++ ))
 	fi 
 done
-<<<<<<< HEAD
 echo "Column name" >> ./${tname}_meta
-=======
-echo "Column;Datatype;PK" >>./$tname
->>>>>>> 67db699cc7a40bd1cebf4230f8b2a16eed8ad7d6
 typeset -i cnumber
 let l=1
-let PK=1 #primary key exist 
+let pk=1 #primary key exist 
 
 read -p "Enter the No. of columns" cnumber
 
 while [ $l -le $cnumber ]
 do
 	echo "Line No.$l"	
-<<<<<<< HEAD
-=======
-	echo "ColumnName"
-	read colname
-	
->>>>>>> 67db699cc7a40bd1cebf4230f8b2a16eed8ad7d6
 	
 	echo "Column name"
 	read metadata
@@ -43,25 +33,12 @@ do
 #check datatype
 	select var in "int" "str"
 	    do
-<<<<<<< HEAD
 	      case $var in
 		"int" ) colType="int";break;;
 		"str" ) colType="str";break;;
-=======
-	    set -x
-	     case $var in 
-		"int" ) 
-		colType="int"
-		 ;;
-		"str" ) 
-		colType="str" 
-		;;
->>>>>>> 67db699cc7a40bd1cebf4230f8b2a16eed8ad7d6
 		* ) echo "Wrong Choice" ;;
 	      esac
-	      
 	    done
-	    
 
 #check primary key
 	 if [[ $PK -eq "" ]]; then
@@ -70,7 +47,6 @@ do
   	        do
 		  case $var in
 		    "y" ) PK="PK";
-<<<<<<< HEAD
 		    echo "Column;Datatype;PK" >> ./${tname}_meta
 		    break;;
 		    "n" ) PK= ""
@@ -81,18 +57,6 @@ do
 	        done
 	  else
 	        echo "Column;Datatype;" " " >> ./${tname}_meta
-=======
-		    echo "Column;Datatype;PK" >>./$tname
-		    ;;
-		    "n" ) PK=""
-                    ;;
-		    * ) echo "This is wrong write another one" ;;
-		  esac
-	        done
-	  else
-	        echo $colname";" $colType";" $PK>>./$tname
-	        echo $colname";" $colType";" $PK>>./${tname}_meta
->>>>>>> 67db699cc7a40bd1cebf4230f8b2a16eed8ad7d6
 	  fi
 
 	  (( l++ ))
@@ -100,15 +64,8 @@ do
           echo metadata >> ./${tname}_meta
 
 
-<<<<<<< HEAD
 done
 
 	
 echo "table created succefully"
 echo "Column;Datatype;PK" >> ./${tname}_meta
-=======
-echo "table created succefully"
-echo "Column;Datatype;PK" >>./$tname
-
-set +x
->>>>>>> 67db699cc7a40bd1cebf4230f8b2a16eed8ad7d6
